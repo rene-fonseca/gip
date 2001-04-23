e02e5cfa932e7ac1f1d863321771249d3275d685
René Fonseca <rene.m.fonseca@gmail.com>
2001-04-23 15:18:59 +0000

    new project started

---
+++ gip/main.cpp
@@ -0,0 +1,19 @@
+/***************************************************************************
+    begin                : Mon Apr 23 17:18:58 CEST 2001
+    copyright            : (C) 2001 by René Møller Fonseca
+    email                : fonseca@mip.sdu.dk
+ ***************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <iostream.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  cout << "Hello, World!" << endl;
+
+  return EXIT_SUCCESS;
+}
