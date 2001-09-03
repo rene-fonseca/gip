f3f7c5d2b2e1d6c6b47665b8bdce5f6c71bd064f
René Fonseca <rene.m.fonseca@gmail.com>
2001-09-03 16:45:42 +0000

    Added gip header.

---
+++ gip/gip.h
@@ -0,0 +1,39 @@
+/***************************************************************************
+    Generic Image Processing (GIP) Framework
+    A framework for developing image processing applications
+
+    Copyright (C) 2001 by René Møller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__GIP__GIP_H
+#define _DK_SDU_MIP__GIP__GIP_H
+
+// include the commonly used header files
+#include <gip/Pixel.h>
+#include <gip/ImageException.h>
+#include <gip/Image.h>
+#include <gip/ArrayImage.h>
+#include <gip/analysis/traverse.h>
+
+using namespace base; // this framework depends on the base framework
+
+namespace gip {
+
+/**
+String getDescription() {
+  return MESSAGE("Generic Image Processing (GIP) Framework\n"
+                 "A framework for developing image processing applications\n"
+                 "Version 1.0\n"
+                 "Copyright (C) 2001 by René Møller Fonseca <fonseca@mip.sdu.dk>");
+}
+*/
+
+}; // end of gip namespace
+
+#endif
