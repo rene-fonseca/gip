a1109201d084854c861f4c2d6fcb964a048fee2a
René Fonseca <rene.m.fonseca@gmail.com>
2001-09-03 17:39:25 +0000

    Added image indirection class.

---
+++ gip/ShadowImage.h
@@ -0,0 +1,56 @@
+/***************************************************************************
+    Generic Image Processing (GIP) Framework
+    A framework for developing image processing applications
+
+    Copyright (C) 2001 by René Møller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__GIP__SHADOW_IMAGE_H
+#define _DK_SDU_MIP__GIP__SHADOW_IMAGE_H
+
+#include <gip/Image.h>
+
+namespace gip {
+
+/**
+  An image that maps onto another image (similar to FilterInputStream and FilterOutputStream).
+
+  @short Shadow image.
+  @author René Møller Fonseca
+*/
+
+template<class PIXEL>
+class ShadowImage : public Image<PIXEL> {
+protected:
+
+  /** The image to work on. */
+  Image<PIXEL>* const image;
+public:
+
+  /**
+    Initializes the shadow image.
+
+    @param image The image.
+  */
+  ShadowImage(Image<PIXEL>* i) throw() : Image<PIXEL>(i->getDimension()), image(i) {}
+
+  /**
+    Returns the dimension of the image.
+  */
+  const Dimension& getDimension() const throw();
+};
+
+template<class PIXEL>
+inline const Dimension& ShadowImage<PIXEL>::getDimension() const throw() {
+  return image->getDimension();
+}
+
+}; // end of namespace
+
+#endif
