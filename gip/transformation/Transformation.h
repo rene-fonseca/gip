3cc07d2d5ffdfd7871981fcb8432eafc5559df91
René Fonseca <rene.m.fonseca@gmail.com>
2001-09-03 16:45:13 +0000

    Added transformation base class.

---
+++ gip/transformation/Transformation.h
@@ -0,0 +1,58 @@
+/***************************************************************************
+    Generic Image Processing (GIP) Framework
+    A framework for developing image processing applications
+
+    Copyright (C) 2001 by René Møller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__GIP_TRANSFORMATION__TRANSFORMATION_H
+#define _DK_SDU_MIP__GIP_TRANSFORMATION__TRANSFORMATION_H
+
+#include <base/Object.h>
+#include <gip/ArrayImage.h>
+
+namespace gip {
+
+/**
+  Transformation.
+
+  @author René Møller Fonseca
+*/
+
+template<class DEST, class SRC>
+class Transformation : public Object {
+public:
+
+  typedef DEST DestinationImage;
+  typedef SRC SourceImage;
+protected:
+
+  /** The destination image of the transformation. */
+  DestinationImage* const destination;
+  /** The source image of the transformation. */
+  const SourceImage* const source;
+public:
+
+  /**
+    Initializes transformation object.
+
+    @param destination The destination image.
+    @param source The source image.
+  */
+  Transformation(DestinationImage* destination, const SourceImage* source) throw();
+};
+
+template<class DEST, class SRC>
+inline Transformation<DEST, SRC>::Transformation(DestinationImage* dest, const SourceImage* src) throw() :
+  destination(dest), source(src) {
+}
+
+}; // end of namespace
+
+#endif
