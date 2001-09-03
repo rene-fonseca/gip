db46f3e6af66b60f5e911c33624452a7387ce897
René Fonseca <rene.m.fonseca@gmail.com>
2001-09-03 16:49:39 +0000

    Added intensity mapping class.

---
+++ gip/transformation/IntensityMapper.h
@@ -0,0 +1,31 @@
+/***************************************************************************
+    Generic Image Processing (GIP) Framework
+    A framework for developing image processing applications
+
+    Copyright (C) 2001 by René Møller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__GIP_TRANSFORMATION__INTENSITY_MAPPER_H
+#define _DK_SDU_MIP__GIP_TRANSFORMATION__INTENSITY_MAPPER_H
+
+#include <gip/transformation/UnaryTransformation.h>
+
+namespace gip {
+
+/**
+  @author René Møller Fonseca
+*/
+//class IntensityMapper : public UnaryTransformation {
+//public:
+//  IntensityMapper();
+//};
+
+}; // end of namespace
+
+#endif
