b522278eb21f5ea44c6b8bfb1960f962183dcb06
René Fonseca <rene.m.fonseca@gmail.com>
2002-03-11 17:53:15 +0000

    Removed getDescription().

--- gip/gip.h
+++ gip/gip.h
@@ -2,7 +2,7 @@
     Generic Image Processing (GIP) Framework
     A framework for developing image processing applications
 
-    Copyright (C) 2001 by René Møller Fonseca <fonseca@mip.sdu.dk>
+    Copyright (C) 2001 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
 
     This framework is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -22,15 +22,6 @@
 #include <gip/analysis/traverse.h>
 
 namespace gip {
-
-/**
-String getDescription() {
-  return MESSAGE("Generic Image Processing (GIP) Framework\n"
-                 "A framework for developing image processing applications\n"
-                 "Version 1.0\n"
-                 "Copyright (C) 2001 by René Møller Fonseca <fonseca@mip.sdu.dk>");
-}
-*/
 
 }; // end of gip namespace
 
