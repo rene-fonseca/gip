5543d5d1d4c42de4a137c631d5d4603722fa37ff
René Fonseca <rene.m.fonseca@gmail.com>
2002-03-17 14:28:54 +0000

    Fixed use of constants and cosmetic changes.

--- gip/transformation/Gradient.cpp
+++ gip/transformation/Gradient.cpp
@@ -2,7 +2,7 @@
     Generic Image Processing (GIP) Framework
     A framework for developing image processing applications
 
-    Copyright (C) 2001 by René Møller Fonseca <fonseca@mip.sdu.dk>
+    Copyright (C) 2001 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
 
     This framework is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,71 +12,69 @@
  ***************************************************************************/
 
 #include <gip/transformation/Gradient.h>
-#include <math.h>
+#include <base/mathematics/constants.h>
 
 namespace gip {
 
-#define M_SQRT2 1.44
+  Gradient::Gradient(DestinationImage* destination, const SourceImage* source) throw(ImageException)
+    : Transformation<DestinationImage, SourceImage>(destination, source) {
+    assert(
+      destination->getDimension() == source->getDimension(),
+      ImageException("Images must have identical dimensions", this)
+    );
+  }
 
-Gradient::Gradient(DestinationImage* destination, const SourceImage* source) throw(ImageException) :
-  Transformation<DestinationImage, SourceImage>(destination, source) {
-  assert(
-    destination->getDimension() == source->getDimension(),
-    ImageException("Images must have identical dimensions")
-  );
-}
+  void Gradient::operator()() throw() {
+    unsigned int rows = destination->getDimension().getHeight();
+    unsigned int columns = destination->getDimension().getWidth();
 
-void Gradient::operator()() throw() {
-  unsigned int rows = destination->getDimension().getHeight();
-  unsigned int columns = destination->getDimension().getWidth();
+    DestinationImage::Rows rowsLookup = destination->getRows();
+    SourceImage::ReadableRows srcRowsLookup = source->getRows();
 
-  DestinationImage::Rows rowsLookup = destination->getRows();
-  SourceImage::ReadableRows srcRowsLookup = source->getRows();
+    DestinationImage::Rows::RowIterator row = rowsLookup.getFirst();
+    ++row;
+    for (unsigned int rowIndex = 1; rowIndex < rows - 1; ++rowIndex) {
 
-  DestinationImage::Rows::RowIterator row = rowsLookup.getFirst();
-  ++row;
-  for (unsigned int rowIndex = 1; rowIndex < rows - 1; ++rowIndex) {
+      SourceImage::ReadableRows::RowIterator srcRow0 = srcRowsLookup[rowIndex - 1];
+      SourceImage::ReadableRows::RowIterator srcRow1 = srcRowsLookup[rowIndex];
+      SourceImage::ReadableRows::RowIterator srcRow2 = srcRowsLookup[rowIndex + 1];
 
-    SourceImage::ReadableRows::RowIterator srcRow0 = srcRowsLookup[rowIndex - 1];
-    SourceImage::ReadableRows::RowIterator srcRow1 = srcRowsLookup[rowIndex];
-    SourceImage::ReadableRows::RowIterator srcRow2 = srcRowsLookup[rowIndex + 1];
+      DestinationImage::Rows::RowIterator::ElementIterator column = row.getFirst();
+      ++column;
 
-    DestinationImage::Rows::RowIterator::ElementIterator column = row.getFirst();
-    ++column;
-
-    for (unsigned int columnIndex = 1; columnIndex < columns - 1; ++columnIndex) {
-      double verticalGray = 0;
-      verticalGray += -M_SQRT2 * *srcRow0[columnIndex - 1];
-      verticalGray += -2 * *srcRow0[columnIndex];
-      verticalGray += -M_SQRT2 * *srcRow0[columnIndex + 1];
+      for (unsigned int columnIndex = 1; columnIndex < columns - 1; ++columnIndex) {
+        long double verticalGray = 0;
+        verticalGray += -constant::SQRT2 * *srcRow0[columnIndex - 1];
+        verticalGray += -2 * *srcRow0[columnIndex];
+        verticalGray += -constant::SQRT2 * *srcRow0[columnIndex + 1];
 
 //      verticalGray += 0 * *srcRow1[columnIndex - 1];
 //      verticalGray += 0 * *srcRow1[columnIndex];
 //      verticalGray += 0 * *srcRow1[columnIndex + 1];
 
-      verticalGray += M_SQRT2 * *srcRow2[columnIndex - 1];
-      verticalGray += 2 * *srcRow2[columnIndex];
-      verticalGray += M_SQRT2 * *srcRow2[columnIndex + 1];
+        verticalGray += constant::SQRT2 * *srcRow2[columnIndex - 1];
+        verticalGray += 2 * *srcRow2[columnIndex];
+        verticalGray += constant::SQRT2 * *srcRow2[columnIndex + 1];
 
-      double horizontalGray = 0;
-      horizontalGray += -M_SQRT2 * *srcRow0[columnIndex - 1];
+        long double horizontalGray = 0;
+        horizontalGray += -constant::SQRT2 * *srcRow0[columnIndex - 1];
 //      horizontalGray += 0 * *srcRow0[columnIndex];
-      horizontalGray += M_SQRT2 * *srcRow0[columnIndex + 1];
+        horizontalGray += constant::SQRT2 * *srcRow0[columnIndex + 1];
 
-      horizontalGray += -2 * *srcRow1[columnIndex - 1];
+        horizontalGray += -2 * *srcRow1[columnIndex - 1];
 //      horizontalGray += 0 * *srcRow1[columnIndex];
-      horizontalGray += 2 * *srcRow1[columnIndex + 1];
+        horizontalGray += 2 * *srcRow1[columnIndex + 1];
 
-      horizontalGray += -M_SQRT2 * *srcRow2[columnIndex - 1];
+        horizontalGray += -constant::SQRT2 * *srcRow2[columnIndex - 1];
 //      horizontalGray += 0 * *srcRow2[columnIndex];
-      horizontalGray += M_SQRT2 * *srcRow2[columnIndex + 1];
+        horizontalGray += constant::SQRT2 * *srcRow2[columnIndex + 1];
 
-//      double gray = fabs(verticalGray) + fabs(horizontalGray);
-      double gray = sqrt(verticalGray*verticalGray + horizontalGray*horizontalGray);
-      *column++ = minimum(static_cast<Intensity>(gray), MAXIMUM_INTENSITY);
+//      long double gray = fabs(verticalGray) + fabs(horizontalGray);
+        long double gray = sqrt(verticalGray*verticalGray + horizontalGray*horizontalGray);
+        *column++ = minimum(static_cast<Intensity>(gray), MAXIMUM_INTENSITY);
+      }
+      ++row;
     }
-    ++row;
   }
-}
 
 }; // end of namespace
