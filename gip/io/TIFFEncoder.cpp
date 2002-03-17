303b076c38f401b038f2bfead7f4c2314115c685
René Fonseca <rene.m.fonseca@gmail.com>
2002-03-17 14:13:02 +0000

    Implemented simple methods.

--- gip/io/TIFFEncoder.cpp
+++ gip/io/TIFFEncoder.cpp
@@ -2,7 +2,7 @@
     Generic Image Processing (GIP) Framework
     A framework for developing image processing applications
 
-    Copyright (C) 2001 by René Møller Fonseca <fonseca@mip.sdu.dk>
+    Copyright (C) 2001 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
 
     This framework is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,7 +15,22 @@
 
 namespace gip {
 
-TIFFEncoder::TIFFEncoder() {
-}
+  TIFFEncoder::TIFFEncoder() throw() {
+  }
+  
+  String TIFFEncoder::getDescription() const throw() {
+    return MESSAGE("Tag Image File Format");
+  }
+  
+  String TIFFEncoder::getDefaultExtension() const throw() {
+    return MESSAGE("tiff");
+  }
+
+  Array<String> TIFFEncoder::getExtensions() const throw() {
+    Array<String> extensions;
+    extensions.append(MESSAGE("tif"));
+    extensions.append(MESSAGE("tiff"));
+    return extensions;
+  }
 
 }; // end of namespace
