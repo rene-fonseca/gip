4d31d25e3d86792027bf436667b710c8f070dd8a
René Fonseca <rene.m.fonseca@gmail.com>
2002-03-17 14:29:24 +0000

    Replaced special characters.

--- gip/transformation/IntensityMapper.h
+++ gip/transformation/IntensityMapper.h
@@ -2,7 +2,7 @@
     Generic Image Processing (GIP) Framework
     A framework for developing image processing applications
 
-    Copyright (C) 2001 by René Møller Fonseca <fonseca@mip.sdu.dk>
+    Copyright (C) 2001 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
 
     This framework is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -19,7 +19,8 @@
 namespace gip {
 
 /**
-  @author René Møller Fonseca
+  @author Rene Moeller Fonseca
+  @version 1.0
 */
 //class IntensityMapper : public UnaryTransformation {
 //public:
