e086e939bf4445aa967ef181acf053e32b478df1
René Fonseca <rene.m.fonseca@gmail.com>
2002-03-17 14:17:34 +0000

    Implementation.

--- gip/io/JPEGEncoder.cpp
+++ gip/io/JPEGEncoder.cpp
@@ -2,7 +2,7 @@
     Generic Image Processing (GIP) Framework
     A framework for developing image processing applications
 
-    Copyright (C) 2001 by René Møller Fonseca <fonseca@mip.sdu.dk>
+    Copyright (C) 2001 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
 
     This framework is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,10 +12,343 @@
  ***************************************************************************/
 
 #include <gip/io/JPEGEncoder.h>
+#include <base/io/File.h>
+#include <base/mem/Allocator.h>
+#include <base/NotImplemented.h>
+
+#include <sys/types.h> // get size_t
+#include <stdio.h> // get FILE (but we do not use this)
+#include <jpeglib.h>
 
 namespace gip {
 
-JPEGEncoder::JPEGEncoder() {
-}
+  class JPEGEncoderImpl {
+  public:
+
+    struct JPEGDestination {
+      struct jpeg_destination_mgr pub;
+      File* file;
+      char* buffer;
+    };
+    
+    static void initializeDestination(j_compress_ptr cinfo) {
+      //JPEGDestination* dest = pointer_cast<JPEGDestination*>(cinfo->dest);
+    }
+
+    static boolean empty(j_compress_ptr cinfo) {
+      JPEGDestination* dest = pointer_cast<JPEGDestination*>(cinfo->dest);
+      dest->file->write(dest->buffer, JPEGEncoder::BUFFER_SIZE);
+      dest->pub.next_output_byte = pointer_cast<JOCTET*>(dest->buffer);
+      dest->pub.free_in_buffer = JPEGEncoder::BUFFER_SIZE;
+      return TRUE;
+    }
+    
+    static void flush(j_compress_ptr cinfo) {
+      JPEGDestination* dest = pointer_cast<JPEGDestination*>(cinfo->dest);
+      dest->file->write(dest->buffer, JPEGEncoder::BUFFER_SIZE - dest->pub.free_in_buffer);
+    }
+    
+    struct JPEGSource {
+      struct jpeg_source_mgr pub;
+      File* file;
+      char* buffer;
+      //bool startOfFile;
+    };
+    
+    static void initialize(j_decompress_ptr cinfo) {
+      //JPEGSource* source = (JPEGSource*)cinfo->src;
+      //source->startOfFile = true;
+    }
+
+    static boolean fill(j_decompress_ptr cinfo) {
+      JPEGSource* source = (JPEGSource*)cinfo->src;
+      unsigned int bytesRead = source->file->read(source->buffer, JPEGEncoder::BUFFER_SIZE, true); // non-blocking
+      if (bytesRead == 0) {
+        bytesRead = source->file->read(source->buffer, JPEGEncoder::BUFFER_SIZE, 1); // blocking
+      }
+      source->pub.next_input_byte = pointer_cast<JOCTET*>(source->buffer);
+      source->pub.bytes_in_buffer = bytesRead;
+      //source->startOfFile = false;
+      return TRUE;
+    }
+
+    static void skip(j_decompress_ptr cinfo, long numberOfBytes) {
+      JPEGSource* source = (JPEGSource*)cinfo->src;
+      if (numberOfBytes > 0) {
+        while (numberOfBytes > static_cast<long>(source->pub.bytes_in_buffer)) {
+          numberOfBytes -= static_cast<long>(source->pub.bytes_in_buffer);
+          fill(cinfo);
+        }
+        source->pub.next_input_byte += (size_t)numberOfBytes;
+        source->pub.bytes_in_buffer -= (size_t)numberOfBytes;
+      }
+    }
+    
+    static void close(j_decompress_ptr cinfo) {
+      // does nothing
+    }
+    
+    static void errorHandler(j_common_ptr cinfo) throw(InvalidFormat) {
+      throw InvalidFormat();
+    }
+    
+  };
+
+  JPEGEncoder::JPEGEncoder() throw() {
+  }
+
+  String JPEGEncoder::getDescription() const throw() {
+    return MESSAGE("Joint Photographic Experts Group");
+  }
+  
+  String JPEGEncoder::getDefaultExtension() const throw() {
+    return MESSAGE("jpg");
+  }
+
+  Array<String> JPEGEncoder::getExtensions() const throw() {
+    Array<String> extensions;
+    extensions.append(MESSAGE("jpg"));
+    extensions.append(MESSAGE("jpeg"));
+    return extensions;
+  }
+
+  bool JPEGEncoder::isValid(const String& filename) throw(IOException) {
+    JPEGEncoderImpl::JPEGSource source;
+    struct jpeg_decompress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+    
+    Allocator<char> buffer(BUFFER_SIZE);
+    File file(filename, File::READ, 0);
+    
+    source.pub.init_source = JPEGEncoderImpl::initialize;
+    source.pub.fill_input_buffer = JPEGEncoderImpl::fill;
+    source.pub.skip_input_data = JPEGEncoderImpl::skip;
+    source.pub.resync_to_restart = ::jpeg_resync_to_restart;
+    source.pub.term_source = JPEGEncoderImpl::close;
+    source.pub.bytes_in_buffer = 0;
+    source.pub.next_input_byte = 0;
+    source.file = &file;
+    source.buffer = buffer.getElements();
+    
+    cinfo.err = ::jpeg_std_error(&jerr);
+    jerr.error_exit = JPEGEncoderImpl::errorHandler;
+    
+    try {
+      ::jpeg_create_decompress(&cinfo);
+      cinfo.src = pointer_cast<struct jpeg_source_mgr*>(&source);
+      try {
+        ::jpeg_read_header(&cinfo, TRUE);
+      } catch(...) {
+        return false;
+      }
+      ::jpeg_destroy_decompress(&cinfo);
+    } catch(...) {
+      return false; // but we really do not know
+    }
+    return true;
+  }
+
+  ColorImage* JPEGEncoder::read(const String& filename) throw(InvalidFormat, IOException) {
+    JPEGEncoderImpl::JPEGSource source;
+    struct jpeg_decompress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+    
+    Allocator<char> buffer(BUFFER_SIZE);
+    File file(filename, File::READ, 0);
+    
+    source.pub.init_source = JPEGEncoderImpl::initialize;
+    source.pub.fill_input_buffer = JPEGEncoderImpl::fill;
+    source.pub.skip_input_data = JPEGEncoderImpl::skip;
+    source.pub.resync_to_restart = ::jpeg_resync_to_restart;
+    source.pub.term_source = JPEGEncoderImpl::close;
+    source.pub.bytes_in_buffer = 0;
+    source.pub.next_input_byte = 0;
+    source.file = &file;
+    source.buffer = buffer.getElements();
+    
+    cinfo.err = ::jpeg_std_error(&jerr);
+    jerr.error_exit = JPEGEncoderImpl::errorHandler;
+    
+    ColorImage* image = 0;
+    try {
+      ::jpeg_create_decompress(&cinfo);
+      cinfo.src = pointer_cast<struct jpeg_source_mgr*>(&source);
+      try {
+        ::jpeg_read_header(&cinfo, TRUE);
+        
+        // we want RGB format (possible gray)
+        cinfo.out_color_space =  JCS_RGB;
+        
+        image = new ColorImage(Dimension(cinfo.image_width, cinfo.image_height));
+        ColorPixel* dest = image->getElements();
+        
+        ::jpeg_start_decompress(&cinfo);
+        
+        JSAMPLE row[cinfo.output_width * cinfo.output_components];
+        JSAMPROW prow = pointer_cast<JSAMPROW>(row);
+        
+        ASSERT(sizeof(JSAMPLE) == 1);
+        
+        while (cinfo.output_scanline < cinfo.output_height) {
+          ::jpeg_read_scanlines(&cinfo, &prow, 1);
+          const JSAMPLE* src = row;
+          const ColorPixel* end = dest + cinfo.output_width;
+          while (dest < end) {
+            ColorPixel element;
+            element.red = *src++;
+            element.green = *src++;
+            element.blue = *src++;
+            *dest++ = element;
+          }
+        }
+        
+        ::jpeg_finish_decompress(&cinfo);
+      } catch(...) {
+        image = 0;
+      }
+      ::jpeg_destroy_decompress(&cinfo);
+    } catch(...) {
+    }
+    
+    return image;
+  }
+  
+  void JPEGEncoder::write(const String& filename, const ColorImage* image) throw(ImageException, IOException) {
+    struct JPEGEncoderImpl::JPEGDestination dest;
+    struct jpeg_compress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+
+    //assert(image->getDimension().getWidth() < 0xffff, ImageException(this));
+    //assert(image->getDimension().getHeight() < 0xffff, ImageException(this));
+
+    Allocator<char> buffer(BUFFER_SIZE);
+    File file(filename, File::WRITE, File::CREATE | File::EXCLUSIVE);
+
+    dest.pub.init_destination = JPEGEncoderImpl::initializeDestination;
+    dest.pub.empty_output_buffer = JPEGEncoderImpl::empty;
+    dest.pub.term_destination = JPEGEncoderImpl::flush;
+    dest.pub.next_output_byte = pointer_cast<JOCTET*>(buffer.getElements());
+    dest.pub.free_in_buffer = BUFFER_SIZE;
+    dest.file = &file;
+    dest.buffer = buffer.getElements();
+    
+    cinfo.err = ::jpeg_std_error(&jerr);
+
+    try {
+      ::jpeg_create_compress(&cinfo);
+      cinfo.dest = pointer_cast<struct jpeg_destination_mgr*>(&dest);
+      
+      cinfo.image_width = image->getDimension().getWidth();
+      cinfo.image_height = image->getDimension().getHeight();
+      cinfo.input_components = 3;
+      cinfo.in_color_space = JCS_RGB;
+      
+      ::jpeg_set_defaults(&cinfo);
+    
+      //::jpeg_set_quality(&cinfo, quality, TRUE /* limit to baseline-JPEG values */);
+      try {
+        ::jpeg_start_compress(&cinfo, TRUE);
+    
+        ASSERT(sizeof(JSAMPLE) == 1);
+        JSAMPLE row[cinfo.image_width * cinfo.num_components];
+        JSAMPROW prow = pointer_cast<JSAMPROW>(row);
+        const ColorPixel* src = image->getElements();
+        
+        while (cinfo.next_scanline < cinfo.image_height) {
+          // TAG: check top-bottom problem
+          JSAMPLE* dest = row;
+          const ColorPixel* end = src + cinfo.image_width;
+          while (src < end) {
+            *dest++ = src->red;
+            *dest++ = src->green;
+            *dest++ = src->blue;
+            ++src;
+          }
+          ::jpeg_write_scanlines(&cinfo, &prow, 1);
+        }
+        ::jpeg_finish_compress(&cinfo);
+      } catch(...) {
+      }
+      ::jpeg_destroy_compress(&cinfo);
+    } catch(...) {
+    }
+  }
+
+  void JPEGEncoder::writeGray(const String& filename, const GrayImage* image) throw(ImageException, IOException) {
+    throw NotImplemented();
+  }
+/*
+  class Information : public Object {
+  private:
+    Dimension dimension;
+    enum Type {GRAY, RGB, RGBA};
+    enum Component {INTENSITY = 1, RED = 2, GREEN = 4, BLUE = 8, ALPHA = 16};
+    unsigned int bitsPerComponent;
+  public:
+
+    Dimension getDimension() const throw() {return dimension;}
+    
+    Type getType() const throw() {return type;}
+
+    unsigned int getPlanes() const throw() {return numberOfComponents;}
+    
+    unsigned int getComponents() const throw() {return components;}
+
+    bool isCompatibleWith(Type type) const throw() {return false;}
+
+    unsigned int bitsPerComponents() const throw() {return bitsPerComponent;}
+
+    unsigned int bitsPerSample() const throw() {return 0;}
+    
+    unsigned int getNumberOfImages() const throw() {return numberOfImages;}
+  };
+  
+  Information getGenralInformation() throw() {
+  }
+  */
+  FormatOutputStream& JPEGEncoder::getInfo(FormatOutputStream& stream, const String& filename) throw(IOException) {
+    JPEGEncoderImpl::JPEGSource source;
+    struct jpeg_decompress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+    
+    Allocator<char> buffer(BUFFER_SIZE);
+    File file(filename, File::READ, 0);
+    
+    source.pub.init_source = JPEGEncoderImpl::initialize;
+    source.pub.fill_input_buffer = JPEGEncoderImpl::fill;
+    source.pub.skip_input_data = JPEGEncoderImpl::skip;
+    source.pub.resync_to_restart = ::jpeg_resync_to_restart;
+    source.pub.term_source = JPEGEncoderImpl::close;
+    source.pub.bytes_in_buffer = 0;
+    source.pub.next_input_byte = 0;
+    source.file = &file;
+    source.buffer = buffer.getElements();
+    
+    cinfo.err = ::jpeg_std_error(&jerr);
+    jerr.error_exit = JPEGEncoderImpl::errorHandler;
+
+    try {
+      ::jpeg_create_decompress(&cinfo);
+      cinfo.src = pointer_cast<struct jpeg_source_mgr*>(&source);
+      try {
+        ::jpeg_read_header(&cinfo, TRUE);
+      } catch(...) {
+        return stream << MESSAGE("JPEGEncoder (Joint Photographic Experts Group File Format):") << EOL
+                      << MESSAGE("  invalid format") << EOL
+                      << EOL;
+      }
+      ::jpeg_destroy_decompress(&cinfo);
+    } catch(...) {
+      return stream << MESSAGE("JPEGEncoder (Joint Photographic Experts Group File Format):") << EOL
+                    << MESSAGE("  invalid format") << EOL
+                    << EOL;
+    }
+    
+    return stream << MESSAGE("JPEGEncoder (Joint Photographic Experts Group File Format):") << EOL
+                  << MESSAGE("  width=") << cinfo.image_width << EOL
+                  << MESSAGE("  height=") << cinfo.image_height << EOL
+                  << MESSAGE("  components=") << cinfo.num_components << EOL
+                  << EOL;
+  }
 
 }; // end of namespace
--- gip/io/PNGEncoder.cpp
+++ gip/io/PNGEncoder.cpp
@@ -2,7 +2,7 @@
     Generic Image Processing (GIP) Framework
     A framework for developing image processing applications
 
-    Copyright (C) 2001 by René Møller Fonseca <fonseca@mip.sdu.dk>
+    Copyright (C) 2001 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
 
     This framework is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,10 +12,270 @@
  ***************************************************************************/
 
 #include <gip/io/PNGEncoder.h>
+#include <base/io/File.h>
+#include <base/NotImplemented.h>
+
+#include <png.h>
 
 namespace gip {
 
-PNGEncoder::PNGEncoder() {
-}
+  class PNGEncoderImpl {
+  public:
+
+    static void read(png_structp context, png_bytep buffer, png_size_t size) {
+      File* file = (File*)::png_get_io_ptr(context);
+      file->read(pointer_cast<char*>(buffer), static_cast<unsigned int>(size)); // could raise exception
+    }
+
+    static void write(png_structp context, png_bytep buffer, png_size_t size) {
+      File* file = (File*)::png_get_io_ptr(context);
+      file->write(pointer_cast<const char*>(buffer), static_cast<unsigned int>(size)); // could raise exception
+    }
+
+    static void flush(png_structp context) throw() {
+      // file.flush(); // TAG: flushed when file is closed
+    }
+
+    static void errorHandler(png_structp context, png_const_charp message) throw(InvalidFormat) {
+      throw InvalidFormat();
+    }
+    
+  };
+  
+  PNGEncoder::PNGEncoder() throw() {
+  }
+  
+  String PNGEncoder::getDescription() const throw() {
+    return MESSAGE("Portable Network Graphics");
+  }
+  
+  String PNGEncoder::getDefaultExtension() const throw() {
+    return MESSAGE("png");
+  }
+
+  bool PNGEncoder::isValid(const String& filename) throw(IOException) {
+    char signature[8]; // size of signature is 8
+    File file(filename, File::READ, 0);
+    unsigned int result = file.read(signature, sizeof(signature));
+    return (result == 8) && !::png_sig_cmp((png_byte*)signature, 0, result);
+  }
+
+  ColorImage* PNGEncoder::read(const String& filename) throw(InvalidFormat, IOException) {
+    File file(filename, File::READ, 0);
+
+    png_structp context = ::png_create_read_struct(
+      PNG_LIBPNG_VER_STRING,
+      0,
+      PNGEncoderImpl::errorHandler,
+      PNGEncoderImpl::errorHandler
+    );
+    assert(context, MemoryException(this));
+    
+    png_infop information = ::png_create_info_struct(context);
+    if (!information) {
+      ::png_destroy_read_struct(&context, 0, 0);
+      throw MemoryException(this);
+    }
+
+    png_infop endInformation = ::png_create_info_struct(context);
+    if (!endInformation) {
+      ::png_destroy_read_struct(&context, &information, 0);
+      throw MemoryException(this);
+    }
+
+    // ::png_set_invert_alpha(context);
+
+    try {
+      ::png_set_read_fn(context, &file, PNGEncoderImpl::read);
+      ::png_read_info(context, information);
+      
+      png_uint_32 width;
+      png_uint_32 height;
+      int bitDepth;
+      int colorType;
+      int interlaceType;
+      int compressionType;
+      int filterType;      
+      ::png_get_IHDR(context, information, &width, &height, &bitDepth, &colorType, &interlaceType, &compressionType, &filterType);
+
+      //if ((colorType == PNG_COLOR_TYPE_RGB) || (colorType == PNG_COLOR_TYPE_RGB_ALPHA)) {
+      //  ::png_set_bgr(context);
+      //}
+
+      if ((colorType == PNG_COLOR_TYPE_GRAY) || (colorType == PNG_COLOR_TYPE_GRAY_ALPHA)) {
+        ::png_set_gray_to_rgb(context);
+      }
+
+      // expand into 32 bits per pixel
+      if ((bitDepth == 8) && (colorType == PNG_COLOR_TYPE_RGB)) {
+        ::png_set_filler(context, ALPHA_OPAQUE_INTENSITY, PNG_FILLER_AFTER);
+      }
+      
+      //if (colorType == PNG_COLOR_TYPE_RGB_ALPHA) {
+      //  ::png_set_swap_alpha(context); // select ARGB
+      //}
+
+      int numberOfPasses = ::png_set_interlace_handling(context);
+      ::png_read_update_info(context, information);
+      ::png_get_IHDR(context, information, &width, &height, &bitDepth, &colorType, &interlaceType, &compressionType, &filterType);
+      
+      if (!((bitDepth == 8) && (colorType == PNG_COLOR_TYPE_RGB))) {
+        ::png_destroy_read_struct(&context, &information, &endInformation);
+        return 0;
+      }
+      
+      ColorImage* image = new ColorImage(Dimension(width, height));
+
+      unsigned int count = height;
+      ColorPixel* rows[1024];
+      ColorPixel* src = image->getElements() + width * height;
+      while (count > 0) {
+        unsigned int blockHeight = minimum(count, 1024U);
+        count -= blockHeight;
+        ColorPixel** row = rows;
+        ColorPixel* end = src - width * blockHeight;
+        while (src > end) {
+          src -= width;
+          *row++ = src;
+        }
+        ::png_read_rows(context, (png_bytepp)rows, 0, blockHeight); // TAG: fix for bad prototype in PNG API
+//        ::png_read_image(context, pointer_cast<png_bytepp>(rows));
+      }
+      
+      ::png_read_end(context, 0);
+      ::png_destroy_read_struct(&context, &information, &endInformation);
+      return image;
+    } catch(...) {
+      ::png_destroy_read_struct(&context, &information, &endInformation);
+      return 0;
+    }
+  }
+
+  void PNGEncoder::write(const String& filename, const ColorImage* image) throw(ImageException, IOException) {
+    unsigned int width = image->getDimension().getWidth();
+    unsigned int height = image->getDimension().getHeight();
+    assert((width <= (1 << 31)) && (height <= (1 << 31)), ImageException(this));
+
+    File file(filename, File::WRITE, File::CREATE | File::EXCLUSIVE);
+    
+    png_structp context = ::png_create_write_struct(
+      PNG_LIBPNG_VER_STRING,
+      0,
+      PNGEncoderImpl::errorHandler,
+      PNGEncoderImpl::errorHandler
+    );
+    assert(context, MemoryException(this));
+    
+    png_infop information = ::png_create_info_struct(context);
+    if (!information) {
+      ::png_destroy_write_struct(&context, 0);
+      throw MemoryException(this);
+    }
+
+    try {
+      ::png_set_write_fn(context, &file, PNGEncoderImpl::write, PNGEncoderImpl::flush);
+      
+      ::png_set_IHDR(
+        context,
+        information,
+        width,
+        height,
+        8,
+        PNG_COLOR_TYPE_RGB,
+        PNG_INTERLACE_NONE,
+        PNG_COMPRESSION_TYPE_DEFAULT,
+        PNG_FILTER_TYPE_DEFAULT
+      );
+      
+      //::png_set_invert_alpha(context);
+      ::png_set_compression_level(context, Z_BEST_COMPRESSION);
+      
+      ::png_write_info(context, information);
+      
+      ::png_set_filler(context, 0, PNG_FILLER_AFTER);
+      //::png_set_bgr(context);
+      
+      unsigned int count = height;
+      const ColorPixel* rows[1024];
+      const ColorPixel* src = image->getElements() + width * height;
+      while (count > 0) {
+        unsigned int blockHeight = minimum(count, 1024U);
+        count -= blockHeight;
+        const ColorPixel** row = rows;
+        const ColorPixel* end = src - width * blockHeight;
+        while (src > end) {
+          src -= width;
+          *row++ = src;
+        }
+        ::png_write_rows(context, (png_bytepp)rows, blockHeight); // TAG: fix for bad prototype in PNG API
+      }
+      
+      ::png_write_end(context, 0); // no comments or other information
+      
+      ::png_destroy_write_struct(&context, &information);
+    } catch(...) {
+      ::png_destroy_write_struct(&context, &information);
+      throw;
+    }
+  }
+  
+  /*
+    need support for encoder specific parameters
+    quality, compression, and more
+    
+    void hint(enum {SLOWEST, DEFAULT, FASTEST}) throw();
+    
+    bool isLossless() const throw() - not static
+  */
+  
+  void PNGEncoder::writeGray(const String& filename, const GrayImage* image) throw(ImageException, IOException) {
+    throw NotImplemented(this);
+  }
+
+  FormatOutputStream& PNGEncoder::getInfo(FormatOutputStream& stream, const String& filename) throw(IOException) {
+    File file(filename, File::READ, 0);
+
+    png_structp context = ::png_create_read_struct(
+      PNG_LIBPNG_VER_STRING,
+      0,
+      PNGEncoderImpl::errorHandler,
+      PNGEncoderImpl::errorHandler
+    );
+    assert(context, MemoryException(this));
+    
+    png_infop information = ::png_create_info_struct(context);
+    if (!information) {
+      ::png_destroy_read_struct(&context, 0, 0);
+      throw MemoryException(this);
+    }
+    
+    try {
+      ::png_set_read_fn(context, &file, PNGEncoderImpl::read);
+      ::png_read_info(context, information);
+      
+      png_uint_32 width;
+      png_uint_32 height;
+      int bitDepth;
+      int colorType;
+      int interlaceType;
+      int compressionType;
+      int filterType;      
+      ::png_get_IHDR(context, information, &width, &height, &bitDepth, &colorType, &interlaceType, &compressionType, &filterType);
+      
+      stream << MESSAGE("PNGEncoder (Portable Network Graphics):") << EOL
+             << MESSAGE("  width=") << width << EOL
+             << MESSAGE("  height=") << height << EOL
+             << MESSAGE("  bit depth=") << bitDepth << EOL
+             << MESSAGE("  color type=") << HEX << PREFIX << colorType << EOL
+             << MESSAGE("  interlace type=") << HEX << PREFIX << interlaceType << EOL
+             << MESSAGE("  compression type=") << HEX << PREFIX << compressionType << EOL
+             << MESSAGE("  filter type=") << HEX << PREFIX << filterType << EOL;
+      
+      ::png_destroy_read_struct(&context, &information, 0);
+    } catch(IOException& e) {
+      ::png_destroy_read_struct(&context, &information, 0);
+    }
+    return stream;
+  }
 
 }; // end of namespace
