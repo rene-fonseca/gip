a308e1e4165b752aff4851cd7f1f04f234eb568b
René Fonseca <rene.m.fonseca@gmail.com>
2002-03-17 14:42:20 +0000

    Added straight line Hough transformation.

---
+++ gip/transformation/StraightLineHoughTransformation.cpp
@@ -0,0 +1,84 @@
+/***************************************************************************
+    Generic Image Processing (GIP) Framework
+    A framework for developing image processing applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#include <gip/transformation/StraightLineHoughTransformation.h>
+#include <base/mathematics/Constants.h>
+
+namespace gip {
+
+  StraightLineHoughTransformation::StraightLineHoughTransformation(DestinationImage* destination, const SourceImage* source) throw(ImageException)
+    : Transformation<DestinationImage, SourceImage>(destination, source) {
+    
+    assert(
+      source->getDimension().isProper(),
+      ImageException("Source image has inproper dimension", this)
+    );
+    assert(
+      destination->getDimension().isProper(),
+      ImageException("Destination image has inproper dimension", this)
+    );
+    
+    Dimension dimension = destination->getDimension();
+    lookup.setSize(dimension.getHeight());
+// TAG: relocateable ???
+// TAG: width % 2 == 1 ???
+    long double deltaTheta = constant::PI/dimension.getHeight();
+    long double inverseOfDeltaRho = dimension.getWidth() *
+      1/sqrt(
+        static_cast<unsigned long long>(dimension.getHeight()) * dimension.getHeight() +
+        static_cast<unsigned long long>(dimension.getWidth()) * dimension.getWidth()
+      );
+    Entry* dest = lookup.getElements();
+    const Entry* end = dest + dimension.getHeight();
+    unsigned int i = 0;
+    while (dest < end) {
+      long double inner = i++ * deltaTheta;
+      dest->cosine = cos(inner) * inverseOfDeltaRho;
+      dest->sine = sin(inner) * inverseOfDeltaRho;
+      ++dest;
+    }
+  }
+
+  void StraightLineHoughTransformation::operator()() throw() {
+    const unsigned int height = destination->getDimension().getHeight();
+    const unsigned int width = destination->getDimension().getWidth();
+    const long double halfWidth = width * 0.5;
+    const Entry* endOfTrigo = lookup.getElements() + height;
+    const int halfSrcHeight = source->getDimension().getHeight()/2;
+    const int halfSrcWidth = source->getDimension().getWidth()/2;
+
+    DestinationImage::Pixel* dest = destination->getElements();
+    fill<DestinationImage::Pixel>(dest, width * height, 0); // reset
+    
+    SourceImage::ReadableRows srcRowLookup = source->getRows();
+    SourceImage::ReadableRows::RowIterator srcRow = srcRowLookup.getFirst();
+    for (int y = -halfSrcHeight; srcRow != srcRowLookup.getEnd(); ++srcRow, ++y) { // traverse all rows
+      SourceImage::ReadableRows::RowIterator::ElementIterator srcColumn = srcRow.getFirst();
+      // TAG: we can precalc y * src->sine here for all theta
+      for (int x = -halfSrcWidth; srcColumn != srcRow.getEnd(); ++srcColumn, ++x) { // traverse all columns of current row
+        // TAG: need predicate support - see functor header
+        if (*srcColumn) {
+          const Entry* trigo = lookup.getElements();
+          DestinationImage::Pixel* destRow = dest;
+          for (int theta = 0; trigo < endOfTrigo; ++trigo, ++theta, destRow += width) { // vote for lines
+            int rho = static_cast<int>(x * trigo->cosine + y * trigo->sine + halfWidth);
+            ASSERT(rho < width);
+            ASSERT(rho >= 0);
+            destRow[rho] += 1; // TAG: alternatively use gradient as weight
+          }
+        }
+      }
+    }
+  }
+  
+}; // end of namespace
---
+++ gip/transformation/StraightLineHoughTransformation.h
@@ -0,0 +1,60 @@
+/***************************************************************************
+    Generic Image Processing (GIP) Framework
+    A framework for developing image processing applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__GIP_TRANSFORMATION__STRAIGHT_LINE_HOUGH_TRANSFORMATION_H
+#define _DK_SDU_MIP__GIP_TRANSFORMATION__STRAIGHT_LINE_HOUGH_TRANSFORMATION_H
+
+#include <gip/gip.h>
+#include <gip/transformation/Transformation.h>
+#include <base/mem/Allocator.h>
+
+namespace gip {
+
+  /**
+    Calculates the straight line Hough transformation of the specified image and
+    stores the result in the destination image.
+
+    @short Straight line Hough transformation
+    @author Rene Moeller Fonseca
+    @version 1.0
+  */
+
+  class StraightLineHoughTransformation : public Transformation<FloatImage, GrayImage> {
+  private:
+
+    struct Entry {
+      long double cosine;
+      long double sine;
+    };
+    
+    /** Lookup table for cosine and sine. */
+    Allocator<Entry> lookup;
+  public:
+
+    /**
+      Initializes the straight line Hough transformation.
+
+      @param destination The destination image.
+      @param source The source image.
+    */
+    StraightLineHoughTransformation(DestinationImage* destination, const SourceImage* source) throw(ImageException);
+
+    /**
+      Calculate transformation.
+    */
+    void operator()() throw();
+  };
+
+}; // end of namespace
+
+#endif
