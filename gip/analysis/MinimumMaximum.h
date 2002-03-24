fa4c0155112931a83c328afcc62405ca27b13dfc
René Fonseca <rene.m.fonseca@gmail.com>
2002-03-24 19:46:53 +0000

    Added minimum/maximum operation.

---
+++ gip/analysis/MinimumMaximum.h
@@ -0,0 +1,65 @@
+/***************************************************************************
+    Generic Image Processing (GIP) Framework
+    A framework for developing image processing applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__GIP_ANALYSIS__MINIMUM_MAXIMUM_H
+#define _DK_SDU_MIP__GIP_ANALYSIS__MINIMUM_MAXIMUM_H
+
+#include <gip/features.h>
+#include <base/Functor.h>
+
+namespace gip {
+  
+  /**
+    This unary operation is used to collect the minimum and maximum values of
+    some group of pixels.
+    
+    @short Unary operation used to find the minimum and maximum values
+    @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+    @version 1.0
+  */
+
+  template<class PIXEL>
+  class MinimumMaximum : public UnaryOperation<PIXEL, void> {
+  private:
+    
+    typedef PIXEL Pixel;
+    
+    Pixel minimumValue;
+    Pixel maximumValue;
+  public:
+    
+    inline MinimumMaximum() throw()
+      : maximumValue(PixelTraits<Pixel>::MINIMUM),
+        minimumValue(PixelTraits<Pixel>::MAXIMUM) {
+    }
+    
+    inline void operator()(const Pixel& value) throw() {
+      if (value > maximumValue) {
+        maximumValue = value;
+      } else if (value < minimumValue) {
+        minimumValue = value;
+      }
+    }
+    
+    inline Pixel getMinimum() const throw() {
+      return minimumValue;
+    }
+    
+    inline Pixel getMaximum() const throw() {
+      return maximumValue;
+    }
+  };
+
+}; // end of gip namespace
+
+#endif
