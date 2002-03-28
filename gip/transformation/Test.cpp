56192c803f379d384b239a7cdc9f1caf14f978ff
René Fonseca <rene.m.fonseca@gmail.com>
2002-03-28 12:49:50 +0000

    Minor updates.

--- gip/transformation/Test.cpp
+++ gip/transformation/Test.cpp
@@ -2,7 +2,7 @@
     Generic Image Processing (GIP) Framework
     A framework for developing image processing applications
 
-    Copyright (C) 2001 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+    Copyright (C) 2001-2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
 
     This framework is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,14 +12,18 @@
  ***************************************************************************/
 
 #include <gip/transformation/Test.h>
+#include <gip/draw/DrawLine.h>
 
 namespace gip {
 
-  Test2::Test2(DestinationImage* destination) throw() : UnaryTransformation<DestinationImage>(destination) {
+  Test::Test(DestinationImage* destination) throw() : UnaryTransformation<DestinationImage>(destination) {
   }
 
-  void Test2::operator()() throw() {
+  void Test::operator()() throw() {
     ColorPixel* element = destination->getElements();
+
+    Canvas canvas(destination);
+    canvas.fill(Point(0, 0), Point(destination->getDimension().getWidth() - 1, destination->getDimension().getHeight() - 1), makeColorPixel(48, 32, 128));
 
     for (unsigned int row = 0; row < destination->getDimension().getHeight(); ++row) {
       for (unsigned int column = 0; column < destination->getDimension().getWidth(); ++column) {
@@ -32,4 +36,4 @@ namespace gip {
     }
   }
 
-}; // end of namespace
+}; // end of gip namespace
--- gip/transformation/Test.h
+++ gip/transformation/Test.h
@@ -2,7 +2,7 @@
     Generic Image Processing (GIP) Framework
     A framework for developing image processing applications
 
-    Copyright (C) 2001 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+    Copyright (C) 2001-2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
 
     This framework is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -11,8 +11,8 @@
     For the licensing terms refer to the file 'LICENSE'.
  ***************************************************************************/
 
-#ifndef _DK_SDU_MIP__GIP__TEST_H
-#define _DK_SDU_MIP__GIP__TEST_H
+#ifndef _DK_SDU_MIP__GIP_TRANSFORMATION__TEST_H
+#define _DK_SDU_MIP__GIP_TRANSFORMATION__TEST_H
 
 #include <gip/transformation/UnaryTransformation.h>
 
@@ -22,17 +22,18 @@ namespace gip {
     Test transformation.
 
     @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+    @ingroup transformations
     @version 1.0
   */
 
-  class Test2 : public UnaryTransformation<ColorImage> {
+  class Test : public UnaryTransformation<ColorImage> {
   public:
-	
-    Test2(DestinationImage* destination) throw();
-	
+
+    Test(DestinationImage* destination) throw();
+
     void operator()() throw();
   };
 
-}; // end of namespace
+}; // end of gip namespace
 
 #endif
