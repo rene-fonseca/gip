d88a3456b11eb2b922a08dd3f25baaa0337ba879
René Fonseca <rene.m.fonseca@gmail.com>
2002-04-16 16:56:57 +0000

    Added RGBPixel support.

--- gip/analysis/MinimumMaximum.h
+++ gip/analysis/MinimumMaximum.h
@@ -60,6 +60,48 @@ namespace gip {
     }
   };
 
+  template<class COMPONENT>
+  class MinimumMaximum<RGBPixel<COMPONENT> > : public UnaryOperation<RGBPixel<COMPONENT>, void> {
+  private:
+
+    typedef RGBPixel<COMPONENT> Pixel;
+
+    Pixel minimumValue;
+    Pixel maximumValue;
+  public:
+
+    inline MinimumMaximum() throw()
+      : maximumValue(makeRGBPixel<COMPONENT>(PixelTraits<Pixel>::MINIMUM, PixelTraits<Pixel>::MINIMUM, PixelTraits<Pixel>::MINIMUM)),
+        minimumValue(makeRGBPixel<COMPONENT>(PixelTraits<Pixel>::MAXIMUM, PixelTraits<Pixel>::MAXIMUM, PixelTraits<Pixel>::MAXIMUM)) {
+    }
+
+    inline void operator()(const Pixel& value) throw() {
+      if (value.red > maximumValue.red) {
+        maximumValue.red = value.red;
+      } else if (value.red < minimumValue.red) {
+        minimumValue.red = value.red;
+      }
+      if (value.green > maximumValue.green) {
+        maximumValue.green = value.green;
+      } else if (value.green < minimumValue.green) {
+        minimumValue.green = value.green;
+      }
+      if (value.blue > maximumValue.blue) {
+        maximumValue.blue = value.blue;
+      } else if (value.blue < minimumValue.blue) {
+        minimumValue.blue = value.blue;
+      }
+    }
+
+    inline Pixel getMinimum() const throw() {
+      return minimumValue;
+    }
+
+    inline Pixel getMaximum() const throw() {
+      return maximumValue;
+    }
+  };
+
 }; // end of gip namespace
 
 #endif
