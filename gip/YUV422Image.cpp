dc0f635f989bbf847eebc0f84724597d1d4cec72
René Fonseca <rene.m.fonseca@gmail.com>
2002-06-16 14:54:58 +0000

    Added YUV 4:2:2 image.

---
+++ gip/YUV422Image.cpp
@@ -0,0 +1,63 @@
+/***************************************************************************
+    Generic Image Processing (GIP) Framework (Test Suite)
+    A framework for developing image processing applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#include <gip/YUV422Image.h>
+
+namespace gip {
+
+  YUV422Image::YUV422Image() throw() {
+  }
+    
+  YUV422Image::YUV422Image(const Dimension& dimension) throw(MemoryException)
+          : Image<uint8>(dimension), y(dimension) {
+    Dimension d = Dimension(dimension.getWidth()/2, dimension.getHeight()/2);
+    u = ArrayImage<uint8>(d);
+    v = ArrayImage<uint8>(d);
+  }
+
+  YUV422Image::YUV422Image(const YUV422Image& copy) throw()
+          : y(copy.y), u(copy.u), v(copy.v) {
+  }
+
+  YUV422Image& YUV422Image::operator=(const YUV422Image& eq) throw() {
+    y = eq.y;
+    u = eq.u;
+    v = eq.v;
+    return *this;
+  }
+
+  ArrayImage<uint8> YUV422Image::getY() throw() {
+    return y;
+  }
+
+  const ArrayImage<uint8> YUV422Image::getY() const throw() {
+    return y;
+  }
+
+  ArrayImage<uint8> YUV422Image::getU() throw() {
+    return u;
+  }
+
+  const ArrayImage<uint8> YUV422Image::getU() const throw() {
+    return u;
+  }
+
+  ArrayImage<uint8> YUV422Image::getV() throw() {
+    return v;
+  }
+  
+  const ArrayImage<uint8> YUV422Image::getV() const throw() {
+    return v;
+  }
+  
+}; // end of namespace gip
---
+++ gip/YUV422Image.h
@@ -0,0 +1,95 @@
+/***************************************************************************
+    Generic Image Processing (GIP) Framework (Test Suite)
+    A framework for developing image processing applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__GIP_MEDIA__YUV_422_IMAGE_H
+#define _DK_SDU_MIP__GIP_MEDIA__YUV_422_IMAGE_H
+
+#include <gip/ArrayImage.h>
+
+namespace gip {
+
+  /**
+    YUV 422 image.
+    
+    @short YUV 422 image.
+    @ingroup images
+    @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+    @version 1.0
+  */
+  class YUV422Image : public Image<uint8> {
+  private:
+
+    /** Y component. */
+    ArrayImage<uint8> y;
+    /** U component. */
+    ArrayImage<uint8> u;
+    /** V component. */
+    ArrayImage<uint8> v;
+  public:
+
+    /**
+      Initializes dummy image.
+    */
+    YUV422Image() throw();
+    
+    /**
+      Initializes YUV 422 image. Raises MemoryException if dimension is invalid.
+
+      @param dimension The dimension of the image.
+    */
+    YUV422Image(const Dimension& dimension) throw(MemoryException);
+
+    /**
+      Initialization of image by image.
+    */
+    YUV422Image(const YUV422Image& copy) throw();
+
+    /**
+      Default assigment of image.
+    */
+    YUV422Image& operator=(const YUV422Image& eq) throw();
+
+    /**
+      Returns the Y component frame.
+    */
+    ArrayImage<uint8> getY() throw();
+
+    /**
+      Returns the Y component frame.
+    */
+    const ArrayImage<uint8> getY() const throw();
+
+    /**
+      Returns the U component frame.
+    */
+    ArrayImage<uint8> getU() throw();
+    
+    /**
+      Returns the U component frame.
+    */
+    const ArrayImage<uint8> getU() const throw();
+
+    /**
+      Returns the V component frame.
+    */
+    ArrayImage<uint8> getV() throw();
+    
+    /**
+      Returns the V component frame.
+    */
+    const ArrayImage<uint8> getV() const throw();
+  };
+  
+}; // end of namespace gip
+
+#endif
