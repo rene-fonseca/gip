2523a12282234e3bd6926242c06290a0d584c943
René Fonseca <rene.m.fonseca@gmail.com>
2019-10-10 16:25:31 +0200

    Added generated header.

---
+++ generated/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define _DK_SDU_MIP__GIT_BRANCH "master"
+#define _DK_SDU_MIP__GIT_COMMIT "7b5f800a9d5d57509a1c5b7a293aeb2c7e9a8483"
+#define _DK_SDU_MIP__GIT_COMMIT_SHORT "7b5f800"
+#define _DK_SDU_MIP__GIT_REVISION 361
